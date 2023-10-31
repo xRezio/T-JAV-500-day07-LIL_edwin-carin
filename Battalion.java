3a710e495a9c62152905f491661ba90f717d7a62
xRezio <edwin.carin@epitech.eu>
2023-10-31 10:28:13 +0100

    first push

--- Battalion.java
+++ Battalion.java
@@ -1,16 +1,45 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
-public class Battalion {
-    private List<Character> characters = new ArrayList<>();
+public class Battalion<T extends Character> {
+    private List<T> characters;
 
-    public void add(List<? extends Character> newCharacters) {
+    public Battalion() {
+        this.characters = new ArrayList<>();
+    }
+
+    public void add(List<T> newCharacters) {
         characters.addAll(newCharacters);
     }
 
     public void display() {
-        for (Character character : characters) {
+        for (T character : characters) {
             System.out.println(character.getName());
         }
+    }
+
+    public boolean fight() {
+        if (characters.size() < 2) {
+            return false;
+        }
+
+        T character1 = characters.get(0);
+        T character2 = characters.get(1);
+
+        int comparisonResult = character1.compareTo(character2);
+
+        if (comparisonResult > 0) {
+            // character1 is the winner, remove character2
+            characters.remove(character2);
+        } else if (comparisonResult < 0) {
+            // character2 is the winner, remove character1
+            characters.remove(character1);
+        } else {
+            // equal, remove both
+            characters.remove(character1);
+            characters.remove(character2);
+        }
+
+        return true;
     }
 }
