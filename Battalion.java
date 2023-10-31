e19468644494f033b370159192e0f15649869583
xRezio <edwin.carin@epitech.eu>
2023-10-31 14:30:20 +0100

    fixed Mage args

--- Battalion.java
+++ Battalion.java
@@ -1,45 +1,44 @@
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
-public class Battalion<T extends Character> {
-    private List<T> characters;
 
-    public Battalion() {
-        this.characters = new ArrayList<>();
+public class Battalion {
+
+  private List<Character> characters;
+
+  public Battalion() {
+    this.characters = new ArrayList<>();
+  }
+
+  public <T extends Character> void add(List<T> characters) {
+    this.characters.addAll(characters);
+  }
+
+  public void display() {
+    for (Character character : characters) {
+      System.out.println(character.getName());
+    }
+  }
+
+  public boolean fight() {
+    if (characters.size() < 2) {
+      return false;
+    }
+    
+    Character first = characters.get(0);
+    Character second = characters.get(1);
+    int result = first.compareTo(second);
+
+    if (result == 0) {
+      this.characters.remove(0);
+      this.characters.remove(0);
+    } else if (result > 0) {
+      this.characters.remove(1);
+    } else {
+      this.characters.remove(0);
     }
 
-    public void add(List<? extends T> newCharacters) {
-        characters.addAll(newCharacters);
-    }
+    return true;
+  }
 
-    public void display() {
-        for (T character : characters) {
-            System.out.println(character.getName());
-        }
-    }
-
-    public boolean fight() {
-        if (characters.size() < 2) {
-            return false;
-        }
-
-        T character1 = characters.get(0);
-        T character2 = characters.get(1);
-
-        int comparisonResult = character1.compareTo(character2);
-
-        if (comparisonResult > 0) {
-            // character1 is the winner, remove character2
-            characters.remove(character2);
-        } else if (comparisonResult < 0) {
-            // character2 is the winner, remove character1
-            characters.remove(character1);
-        } else {
-            // equal, remove both
-            characters.remove(character1);
-            characters.remove(character2);
-        }
-
-        return true;
-    }
 }
